b260158cd47aa9bfd2abc85b4b9a609f26aa3159
Steven Washington <swashington@connellypartners.com>
2019-01-28 13:50:41 -0500

    fixing striptags function

--- gatsby/src/utils/index.js
+++ gatsby/src/utils/index.js
@@ -1,5 +1,31 @@
+export const htmlentities = {
+  // FROM: https://ourcodeworld.com/articles/read/188/encode-and-decode-html-entities-using-pure-javascript
+  /*
+   * Converts a string to its html characters completely.
+   *
+   * @param {String} str String with unescaped HTML characters
+   **/
+  encode : function(str) {
+    var buf = [];
+    
+    for (var i=str.length-1;i>=0;i--) {
+      buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
+    }
+    
+    return buf.join('');
+  },
+  /**
+   * Converts an html characterSet into its original character.
+   *
+   * @param {String} str htmlSet entities
+   **/
+  decode : function(str) {
+    return str.replace(/&#(\d+);/g, function(match, dec) {
+      return String.fromCharCode(dec);
+    });
+  }
+};
+
 export const stripTags = s => {
-  const d = document.createElement('div');
-  d.innerHTML = s;
-  return d.textContent || d.innerText || "";
+  return htmlentities.decode(s.replace(/<[^>]+>/g, ""));
 }
