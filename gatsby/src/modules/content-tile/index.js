898c4fef7c989c726f9d30ee03fd5d6af140f78c
Steven Washington <swashington@connellypartners.com>
2019-02-04 16:46:55 -0500

    Content tile module front end

--- gatsby/src/modules/content-tile/index.js
+++ gatsby/src/modules/content-tile/index.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import { graphql } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import './main.scss';
 
 class ContentTile extends Component {
   render() {
+    const {
+      eyebrow,
+      headline,
+      supportive_text,
+      cta,
+      image: {
+        localFile: {
+          childImageSharp: {
+            original: { src: contentTileImage },
+          },
+        },
+      },
+    } = this.props;
     return (
-      <div>
-        Content Tile Module
-        <pre><code>{JSON.stringify(this.props, null, 1)}</code></pre>
-      </div>
+      <section
+        className="latest bg-dark bg-img page-sec"
+        style={{backgroundImage:`url(${contentTileImage})`}}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <h4 className="eyebrow">{eyebrow}</h4>
+              <h2>
+                {headline}
+                <span className="highlight">.</span>
+              </h2>
+              <p>
+                {supportive_text}
+              </p>
+              <Link to={cta.url} title={cta.title} className="cta">
+                {cta.title}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
