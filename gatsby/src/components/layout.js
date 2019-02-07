b601340be5f4f963453113db12ddf8a696c82525
Steven Washington <swashington@connellypartners.com>
2019-02-07 12:11:40 -0500

    Footer

--- gatsby/src/components/layout.js
+++ gatsby/src/components/layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MainNav from '../modules/main-nav';
-import Footer from './footer';
+import Footer from '../modules/footer';
 import '../modules/_global/scss/global.scss';
 
 class Layout extends Component {
