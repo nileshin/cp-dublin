a5f9c7a36e8f3efff4d626b353de04064b36ab61
Eric Webster <ewebster@connellypartners.com>
2019-02-09 11:24:21 -0500

    moved JS into module for time being

--- gatsby/src/modules/capabilities-tiles/index.js
+++ gatsby/src/modules/capabilities-tiles/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
-
+import $ from 'jquery';
 import './main.scss';
-//import './main.js';
+
 
 import { ReactComponent as Hamburger} from '../_global/images/hamburger-close.svg';
 import { ReactComponent as DownloadIcon} from '../_global/images/icon-dwnld.svg';
@@ -11,9 +11,61 @@ import { ReactComponent as PlusIcon} from '../_global/images/icon-plus.svg';
 
 class CapabilitiesTiles extends Component {
   componentDidMount() {
+    var popTarget, targetData;
 
-    
+    $(document).ready(function() {
+      
+      openPopup();
+      $(document).keydown(function (e) {
+        if (e.keyCode === 27) {
+          closePopup();
+        }
+      });
+      $('body').on('click touchstart', function (e) {
+        if ($(".pop-up").is(":visible")) {
+          if ($(e.target).closest(".popWrap").length === 0) {
+            closePopup();
+          }
+        }
+      });
 
+      $(".close").click(function() {
+        closePopup();
+      });
+
+
+
+    });
+
+    function openPopup() {
+      $(document).on('click', '.pop-up__btn', function (e) {
+        e.preventDefault();
+        if ($(this).is('[data-href]')) {
+          popTarget = $(this).data("href");
+        } else {
+          popTarget = $(this).attr("href");
+        }
+        targetData = $(popTarget)[0].outerHTML;
+        $(popTarget).wrap("<span class='pop-up__placeholder'></span>")
+        $(popTarget).prependTo(".pop-up__content");
+        $(".pop-up__content > *").addClass("popWrap");
+        $(".pop-up").fadeIn();
+        $("body").addClass("popOpen");
+      });
+    }
+
+    function closePopup() {
+      if ($(".pop-up").is(":visible")) {
+        $(".pop-up").fadeOut(function () {
+          $("body").removeClass("popOpen");
+          $(".pop-up__content > *").removeClass("popWrap");
+          $(popTarget).appendTo(".pop-up__placeholder");
+          $(popTarget).unwrap();
+          $(".pop-up__content").html("");
+        });
+      }
+    }
+ 
   }
   render() {
     const {title, services_and_capabilities_pdf, capabilities} = this.props;
