2f71a79cd4ffa2b2154b1af29c043c58e1158177
Eric Webster <ewebster@connellypartners.com>
2019-02-10 08:45:16 -0500

    added work detail and stat long fact module

--- gatsby/src/modules/stat-long-fact-row/index.js
+++ gatsby/src/modules/stat-long-fact-row/index.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 import { graphql } from 'gatsby';
 
+import './main.scss';
+
 class StatLongFactRow extends Component {
   render() {
+    const { statistic, fact } = this.props;
     return (
-      <div style={{border:"solid 1px #700bff"}}>
-        Stat Long Fact Row
-        <pre><code>{JSON.stringify(this.props, null, 1)}</code></pre>
-      </div>
+      <section className="stat">
+        <div style={{display: "none", border:"solid 1px #700bff"}}>
+          Stat Long Fact Row
+          <pre><code>{JSON.stringify(this.props, null, 1)}</code></pre>
+        </div>
+        <div className="container">
+          <div className="row stat__head">
+            <div className="col-5 col-md-4">
+              <h3 className="stat__title" dangerouslySetInnerHTML={{
+                __html: statistic.stat_title,
+              }} />
+            </div>
+            <div className="col-7 col-md-8">
+              <h3 className="stat__title" dangerouslySetInnerHTML={{
+                __html: fact.fact_title,
+              }} />
+            </div>
+          </div>
+          <div className="row stat__content">
+            <div className="col-5 col-md-4">
+              <span className="lg-text">{statistic.stat_number_unit}</span>
+            </div>
+            <div className="col-7 col-md-8">
+
+            {fact.fact_content.map((fact_row, index) => 
+              <article className="fact">
+                <h3 className="fact__title">
+                  {fact_row.fact_text}
+                </h3>
+              </article>
+            )} 
+            </div>
+          </div>
+        </div>        
+      </section>
     );
   }
 }
--- gatsby/src/modules/work-detail-intro/index.js
+++ gatsby/src/modules/work-detail-intro/index.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
+// import { nominalTypeHack } from 'prop-types';
+
+import './main.scss'
 
 class WorkDetailIntro extends Component {
   render() {
+    const { title, body_copy, image } = this.props;
+
     return (
-      <div style={{ border: 'solid 1px #bada55' }}>
-        Work Detail Intro
-        <pre>
-          <code>{JSON.stringify(this.props, null, 1)}</code>
-        </pre>
-      </div>
+      <section className="intro-work">
+        <div style={{ display: 'none', border: 'solid 1px #bada55' }}>
+          Work Detail Intro
+          <pre>
+            <code>{JSON.stringify(this.props, null, 1)}</code>
+          </pre>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="intro-work__content">
+                <h1 dangerouslySetInnerHTML={{
+                  __html: title,
+                }}/>
+                <div dangerouslySetInnerHTML={{
+                  __html: body_copy,
+                }}/>
+              </div>
+            </div>
+            <div className="col-md-6 intro-work__img">
+              <figure>
+                <Img
+                  fluid={image.localFile.childImageSharp.fluid}
+                  alt={image.alt_text}
+                  className="cover"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
@@ -21,7 +52,7 @@ export const workDetailIntroFragment = graphql`
     title
     body_copy
     image {
-      ...WpMediaFragment
+      ...WpMediaFragmentFluid1440
     }
   }
 `;
