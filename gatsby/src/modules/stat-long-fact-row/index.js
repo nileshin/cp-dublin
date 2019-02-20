6f2924194454b013e608fd5f612210fe19732c38
Steven Washington <washington.steven@gmail.com>
2019-02-19 20:16:00 -0500

    Fixing missing client name CP30-26

--- gatsby/src/modules/stat-long-fact-row/index.js
+++ gatsby/src/modules/stat-long-fact-row/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'gatsby';
-import { ReactComponent as NewTabIcon } from '../_global/images/icon-new-tab.svg'; 
+import { ReactComponent as NewTabIcon } from '../_global/images/icon-new-tab.svg';
 
 import './main.scss';
 
@@ -38,37 +38,41 @@ class StatLongFactRow extends Component {
             <div className="col-5 col-md-4">
               <span className="lg-text">{statistic.stat_number_unit}</span>
             </div>
-            <div className="col-7 col-md-8">
-              {fact.fact_content.map((fact_row, index) => (
-                <article className="fact" key={index}>
-                  <h3 className="fact__title">
-                    {fact_row.fact_link ? (
-                      <a
-                        href={fact_row.fact_link.url}
-                        target={fact_row.fact_link.target}
-                      >
-                        {fact_row.fact_text}
-                      </a>
-                    ) : (
-                      fact_row.fact_text
+            {fact.fact_content && (
+              <div className="col-7 col-md-8">
+                {fact.fact_content.map((fact_row, index) => (
+                  <article className="fact" key={index}>
+                    <h3 className="fact__title">
+                      {fact_row.fact_link ? (
+                        <a
+                          href={fact_row.fact_link.url}
+                          target={fact_row.fact_link.target}
+                        >
+                          {fact_row.fact_text}
+                        </a>
+                      ) : (
+                        fact_row.fact_text
+                      )}
+                    </h3>
+                    {fact_row.fact_link && (
+                      <div className="fact__link">
+                        <a
+                          href={fact_row.fact_link.url}
+                          target={fact_row.fact_link.target}
+                          title={fact_row.fact_link.title}
+                        >
+                          {fact_row.fact_link.target === '_blank' && (
+                            <NewTabIcon className="icn" />
+                          )}
+                          {fact_row.fact_link.title}
+                        </a>
+                        <time>{fact_row.fact_link_supporting_text}</time>
+                      </div>
                     )}
-                  </h3>
-                  {fact_row.fact_link && (
-                    <div className="fact__link">
-                      <a
-                        href={fact_row.fact_link.url}
-                        target={fact_row.fact_link.target}
-                        title={fact_row.fact_link.title}
-                      >
-                        {fact_row.fact_link.target === "_blank" && <NewTabIcon className="icn" />}
-                        {fact_row.fact_link.title}
-                      </a>
-                      <time>{fact_row.fact_link_supporting_text}</time>
-                    </div>
-                  )}
-                </article>
-              ))}
-            </div>
+                  </article>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </section>
