a74251cd5a215c3d64954acfff4e61110e623ae7
Steven Washington <swashington@connellypartners.com>
2019-03-01 16:05:51 -0500

    Adding proper link tag for fonts

--- gatsby/src/components/layout.js
+++ gatsby/src/components/layout.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import MainNav from '../modules/main-nav';
 import Footer from '../modules/footer';
+import Helmet from 'react-helmet';
 import '../modules/_global/scss/global.scss';
 
 class Layout extends Component {
   render() {
     return (
       <>
+        <Helmet>
+          <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6014912/7241012/css/fonts.css" />
+        </Helmet>
         <MainNav />
         <main>{this.props.children}</main>
         <Footer />
