f6237921dd6174dca1f05c0cea9b2cbcde67a619
Steven Washington <swashington@connellypartners.com>
2019-05-09 11:20:22 -0400

    fixing build error

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 // You can delete this file if you're not using it
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   return new Promise((resolve, reject) => {
     graphql(
       `
@@ -25,112 +25,128 @@ exports.createPages = ({ graphql, actions }) => {
           }
         }
       `
-    ).then(result => {
-      if (result.errors) {
-        console.error(result.errors)
-        reject(result.errors)
-      }
+    )
+      .then(result => {
+        if (result.errors) {
+          console.error(result.errors);
+          reject(result.errors);
+        }
 
-      const pageTemplate = path.resolve('./src/templates/page.js')
-      result.data.allWordpressPage.edges.forEach(({ node: page }) => {
-        if (fs.existsSync(path.resolve(`./src/pages/${page.slug}.js`))) {
-          return;
-        } if (page.slug === 'home') {
-          // The slug for the homepage is "home", which doesn't match "index"
-          // So we need this special check
-          // All other page slugs / ${page}.js names should match as a rule.
-          return;
-        }
-        createPage({
-          path: `/${page.slug}/`,
-          component: pageTemplate,
-          context: {
-            id: page.id,
-          },
-        })
-      })
-    }).then(() => {
-      return graphql(`
-        {
-          allWordpressWpWork {
-            edges {
-              node {
-                id
-                wordpress_id
-                slug
-                status
-                type
-              }
-            }
+        const pageTemplate = path.resolve('./src/templates/page.js');
+        result.data.allWordpressPage.edges.forEach(({ node: page }) => {
+          if (fs.existsSync(path.resolve(`./src/pages/${page.slug}.js`))) {
+            return;
           }
-        }
-      `)
-    }).then(result => {
-      if (result.errors) {
-        console.error(result.errors)
-        reject(result.errors)
-      }
-
-      const workDetailTemplate = path.resolve('./src/templates/work-detail.js');
-      result.data.allWordpressWpWork.edges.forEach(({ node: page }) => {
-        if (fs.existsSync(path.resolve(`./src/pages/${page.slug}.js`))) {
-          return;
-        }
-        createPage({
-          path: `/${page.type}/${page.slug}/`,
-          component: workDetailTemplate,
-          context: {
-            id: page.id
+          if (page.slug === 'home') {
+            // The slug for the homepage is "home", which doesn't match "index"
+            // So we need this special check
+            // All other page slugs / ${page}.js names should match as a rule.
+            return;
           }
+          createPage({
+            path: `/${page.slug}/`,
+            component: pageTemplate,
+            context: {
+              id: page.id,
+            },
+          });
         });
       })
-    }).then(() => {
-      return graphql(`
-        {
-          allWordpressPost {
-            edges {
-              node {
-                id
-                slug
-                acf {
-                  internal_external
+      .then(() => {
+        return graphql(`
+          {
+            allWordpressWpWork {
+              edges {
+                node {
+                  id
+                  wordpress_id
+                  slug
+                  status
+                  type
                 }
               }
             }
           }
+        `);
+      })
+      .then(result => {
+        if (result.errors) {
+          console.error(result.errors);
+          reject(result.errors);
         }
-      `)
-    }).then(result => {
-      const newsDetailTemplate = path.resolve('./src/templates/news-detail.js');
-      result.data.allWordpressPost.edges.forEach(({node: page}) => {
-        if (page.acf.internal_external !== 'internal') return;
-        createPage({
-          path: `/news/${page.slug}`,
-          component: newsDetailTemplate,
-          context: {
-            id: page.id
-          }
-        })
-      });
-      resolve();
-    });
-  })
-}
 
-exports.onCreateWebpackConfig = ({stage, rules, loaders, plugins, actions}) => {
+        const workDetailTemplate = path.resolve(
+          './src/templates/work-detail.js'
+        );
+        result.data.allWordpressWpWork.edges.forEach(({ node: page }) => {
+          if (fs.existsSync(path.resolve(`./src/pages/${page.slug}.js`))) {
+            return;
+          }
+          createPage({
+            path: `/${page.type}/${page.slug}/`,
+            component: workDetailTemplate,
+            context: {
+              id: page.id,
+            },
+          });
+        });
+      })
+      .then(() => {
+        return graphql(`
+          {
+            allWordpressPost {
+              edges {
+                node {
+                  id
+                  slug
+                  acf {
+                    internal_external
+                  }
+                }
+              }
+            }
+          }
+        `);
+      })
+      .then(result => {
+        const newsDetailTemplate = path.resolve(
+          './src/templates/news-detail.js'
+        );
+        result.data.allWordpressPost.edges.forEach(({ node: page }) => {
+          if (page.acf.internal_external !== 'internal') return;
+          createPage({
+            path: `/news/${page.slug}`,
+            component: newsDetailTemplate,
+            context: {
+              id: page.id,
+            },
+          });
+        });
+        resolve();
+      });
+  });
+};
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  loaders,
+  actions,
+}) => {
+  let loader = 'imports-loader?this=>window,fix=>module.exports=0';
+  if (stage === 'build-html') loader = loaders.null();
   actions.setWebpackConfig({
     module: {
       rules: [
         {
           test: require.resolve('snapsvg/dist/snap.svg.js'),
-          use: 'imports-loader?this=>window,fix=>module.exports=0',
+          use: loader,
         },
-      ]
+      ],
     },
     resolve: {
       alias: {
         snapsvg: 'snapsvg/dist/snap.svg.js',
       },
     },
-  })
-}
+  });
+};
